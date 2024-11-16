2667f1efb7505f391d394cbb5a28b15db1fbc204
Dmitry Zavodnikov <d.zavodnikov@gmail.com>
2024-11-16 19:17:45 +0100

    Create template

---
+++ src/main/java/pro/zavodnikov/generator/FlatArrayFactory.java
@@ -0,0 +1,51 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2009-2024 Dmitry Zavodnikov
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package pro.zavodnikov.generator;
+
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * Flat Array factory.
+ */
+public class FlatArrayFactory<T> extends FlatFactory<T> {
+
+    public FlatArrayFactory() {
+        super("FlatArray");
+    }
+
+    public T createArray(final Class<T> cl, final int length) {
+        try {
+            return cl.cast(getImpl(cl).getDeclaredConstructor(int.class).newInstance(length));
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException
+                | NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected Class<?> createImpl(final ClassLoader classLoader, final Class<T> classDef) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createImpl'");
+    }
+}
